3ce729e0003b88c717b670028cee00dc6f78844b
manhnd <manhnd@neo.vn>
2025-09-08 03:01:21 +0700

    first commit

---
+++ spring-framework/session06-springboot/src/main/java/dev/m/Session06SpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session06SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session06SpringbootApplication.class, args);
+    }
+
+}
---
+++ spring-framework/session07-springmvc/src/main/java/dev/m/Session07SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package dev.m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session07SpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session07SpringmvcApplication.class, args);
+    }
+
+}
